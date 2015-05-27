b02ec1e199822af99e0bde45c76b69d09fbdd0a6
SunjoongYi <geodesic@nate.com>
2015-05-27 18:46:50 +0900

    Background Music_Service Foreground -> Background music Relay Function Some Bug Fix Background -> Foreground Movie Relay Function Add SeekBar

--- src/com/example/videoplayer/MainActivity.java
+++ src/com/example/videoplayer/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.videoplayer;
 
+import com.example.videoplayer.PlayService.LocalBinder;
+//import com.example.videoplayer.PlayService.MyBinder;
+import com.example.videoplayer.aidl.*;
+import com.google.gson.Gson;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+
+
+
+
+import android.media.MediaPlayer.OnPreparedListener;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,13 +32,18 @@ import android.widget.LinearLayout;
 import android.widget.VideoView;
 import android.widget.MediaController;
 import android.widget.Button;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,26 +54,55 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
+
+/////////////////////////////////////////////////////
 
 public class MainActivity extends Activity {
-	private VideoView videoview;
-	private MediaController mc;
+	SharedPreferences mPrefs = null;	
 	
-	private Button back_Button;
-	private Button home_Button;
-	private Button menu_Button;
-	private Button function_Button;
-	private Button setting_Button;
+	private TextView timeText1;
+	private TextView timeText2;
 	
-	private Button play_Button;
-	private Button ff_Button;
-	private Button rw_Button;
-	private Button list_Button;
+	private int current_video_hours = 0;		
+	private int current_video_minutes = 0;
+	private int current_video_seconds = 0;
+	
+	private int current_position = 0;
+	private boolean mBound = false;
+	private String filename = null;
+	private Intent intent;
+	
+	private static boolean isBackFromHomeKey = false;
+	private PlayService playservice;	// 액티비티가 백그라운드로 갈 때 소리를 재생시키는 서비스
+	private VideoView videoview;		//  
+	
+	private MediaController mc;			// 미디어 컨트롤러 (사용 X, 자체 UI 제작)
+	
+	private Button back_Button;			// BACK 버튼
+	private Button home_Button;			// HOME 버튼
+	private Button menu_Button;			// MENU 버튼
+	private Button function_Button;		// FUNCTION 버튼
+	private Button setting_Button;		// SETTING 버튼
+	
+	private Button play_Button;			// PLAY 버튼
+	private Button ff_Button;			// FF 버튼
+	private Button rw_Button;			// RW 버튼
+	private Button list_Button;			// 리스트 버튼
 	
 	private FrameLayout FrameLayout;
 	private LinearLayout LinearLayout;
 	private LinearLayout List;
+	
+	private SeekBar seekBar;
+	private SeekBar.OnSeekBarChangeListener soundcontrolListener;
+	
+	private Runnable onEverySecond = null;
+	
+	private int First_Flag = 0; 
 	
 	static final private int list_Flag = 0;
 	static final private int setting_Flag = 1;
@@ -72,18 +120,90 @@ public class MainActivity extends Activity {
 	String[] thumbColumns = {MediaStore.Video.Thumbnails.DATA,
 			MediaStore.Video.Thumbnails.VIDEO_ID };
 	
+	
+	//////////////////
+	private ServiceConnection serviceConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			 
+			mBound = false;
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			LocalBinder binder = (LocalBinder) service;
+			playservice = binder.getService();
+			mBound = true;
+		}
+	};
+
 	//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(MainActivity.this, "onCreate()", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
-        //setContentView(R.layout.listitem);
+        
+        mPrefs = getPreferences(MODE_PRIVATE);
+        if(savedInstanceState == null){
+        	
+        	SeekBar.OnSeekBarChangeListener soundcontrolListener = new SeekBar.OnSeekBarChangeListener() {
+        		
+        		@Override
+        		public void onStopTrackingTouch(SeekBar seekBar) {
+        			// TODO Auto-generated method stub
+        			
+        		}
+        		
+        		@Override
+        		public void onStartTrackingTouch(SeekBar seekBar) {
+        			// TODO Auto-generated method stub
+        			
+        		}
+        		
+        		@Override
+        		public void onProgressChanged(SeekBar seekBar, int progress,
+        				boolean fromUser) {
+        			// TODO Auto-generated method stub
+
+       				if(fromUser){
+       					videoview.seekTo(progress);
+       				}
+       				
+       				int seconds = (int)(progress/1000);
+       				int minutes = (int)(seconds/60);
+       				int hours = (int)(seconds/3600);
+       				
+       				String time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+       				timeText1.setText(time);
+       				
+        		}
+        	};        	
+        
+        //intent = new Intent(this, PlayService.class);
+        
+        //stopService(intent);
+
 
         //VideoView (videoview), MediaController (mc)객체 초기화
-        videoview = (VideoView)this.findViewById(R.id.VideoView1);
+        videoview = (VideoView)this.findViewById(R.id.VideoView1);//
+        videoview.setKeepScreenOn(true);
+        
+        //playservice.setVideoview((VideoView)this.findViewById(R.id.VideoView1));
         mc = new MediaController(this);
         
         //Flag 초기화
+        
+        //SeekBar 초기화
+        seekBar = (SeekBar)findViewById(R.id.SeekBar1);
+        seekBar.setOnSeekBarChangeListener(soundcontrolListener);
+        
+        //TextView 초기화
+        this.timeText1 = (TextView)findViewById(R.id.TimeText1);
+        this.timeText2 = (TextView)findViewById(R.id.TimeText2);
         
         //버튼 초기화
         back_Button = (Button)findViewById(R.id.Button01);
@@ -109,8 +229,11 @@ public class MainActivity extends Activity {
         	button_Array[i].requestFocus();
         }
         
-        videoview.setMediaController(null);
+        // 버튼 초기화 끝
+        
 
+               
+        // BACK 버튼 클릭시 이벤트
         back_Button.setOnClickListener(new OnClickListener(){
         	public void onClick(View v){
         		
@@ -142,16 +265,48 @@ public class MainActivity extends Activity {
         			}
         	}
         });
-
+        
+        
+        onEverySecond = new Runnable(){
+        	@Override
+        	public void run(){
+        		if(seekBar != null){
+        			seekBar.setProgress(videoview.getCurrentPosition());
+        		}
+        		if(videoview.isPlaying()){
+        			seekBar.postDelayed(onEverySecond, 1000);
+        		}
+        	}
+        };
+        
+        // PLAY 버튼 클릭시 이벤트
         play_Button.setOnClickListener(new OnClickListener(){
         	public void onClick(View v){
         		
         			play_Button.setSelected(true);
-        			play_Button.setPressed(true);        			
-        			videoview.start();
+        			play_Button.setPressed(true);
+        			
+        			videoview.requestFocus();
+        			videoview.setOnPreparedListener(new OnPreparedListener() {
+						
+						@Override
+						public void onPrepared(MediaPlayer mp) {
+							// TODO Auto-generated method stub
+							seekBar.setMax(videoview.getDuration());
+							//seekBar.postDelayed(onEverySecond, 1000);
+							Toast.makeText(MainActivity.this, Integer.toString(videoview.getDuration()), Toast.LENGTH_LONG).show();
+							videoview.start();
+							//onEverySecond.run();
+							
+						}
+					});
+        			//videoview.start();
+        			//startService(new Intent(MainActivity.this, PlayService.class));
         	}
         });
         
+        
+        // LIST 버튼 클릭시 이벤트
         list_Button.setOnClickListener(new OnClickListener(){
         	public void onClick(View v){
         			list_Button.setSelected(true);
@@ -161,19 +316,21 @@ public class MainActivity extends Activity {
         	}
         });
         
-        //videoview 객체에 mc 객체 부착
+        // seekbar
+        
+        //videoview 객체에 mc 객체 부착 -> 현재는 mc 를 이용하지 않으므로 매개변수에 null 대입
+        videoview.setMediaController(null);
         
         //내부 저장소의 위치를 얻어온다.        
         String folder = Environment.getExternalStorageDirectory().getAbsolutePath();
 
         //내부 저장소의 trailer.mp4 를 재생 파일로 지정
-        videoview.setVideoPath(folder + "/trailer.mp4");
-
+        //videoview.setVideoPath(folder + "/trailer.mp4");
+        
         //포커스를 맞춘다.
         videoview.requestFocus();
         
         //비디오를 재생한다.
-            
         this.getWindow().getDecorView().
         setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
         
@@ -186,16 +343,43 @@ public class MainActivity extends Activity {
         List.setVisibility(View.INVISIBLE);
 
         LinearLayout.setMeasureWithLargestChildEnabled(true);
-        LinearLayout.setVisibility(View.VISIBLE);      
+        LinearLayout.setVisibility(View.VISIBLE);    
+        
+        //videoview.start();
+        
+        }
+        else{
+        	Toast.makeText(MainActivity.this, "onCreate else()", Toast.LENGTH_LONG).show();
+    		filename = savedInstanceState.getString("videopath");
+    		videoview.setVideoPath(filename);
+    		
+    		Gson gson = new Gson();
+    		String json = mPrefs.getString("intent", "");
+    		intent = gson.fromJson(json, Intent.class);
+    		
+    		stopService(intent);
+    		
+    		this.filename = playservice.getFilePath();
+    		this.current_position = playservice.getSeekTime();
+    		
+    		videoview.setVideoPath(filename);
+    		videoview.seekTo(current_position);
+			videoview.requestFocus();
+			videoview.setOnPreparedListener(new OnPreparedListener() {
+				
+				@Override
+				public void onPrepared(MediaPlayer mp) {
+					// TODO Auto-generated method stub
+					seekBar.setMax(videoview.getDuration());
+					//seekBar.postDelayed(onEverySecond, 1000);
+					videoview.start();
+					//onEverySecond.run();
+					
+				}
+			});    		
+    		//videoview.start();
+        }
     }
-    
-   
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		videoview.stopPlayback();
-	}
 	
 	@SuppressWarnings("deprecation")
 	private void init_phone_video_grid() {
@@ -223,7 +407,25 @@ public class MainActivity extends Activity {
 			video_column_index = videocursor
 					.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
 			videocursor.moveToPosition(position);
-			String filename = videocursor.getString(video_column_index);
+			filename  = videocursor.getString(video_column_index);
+			videoview.setVideoPath(filename);
+			
+			video_column_index = videocursor
+					.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+			String iduration = videocursor.getString(video_column_index);
+			
+			long timeInmillisec = Long.parseLong(iduration);
+			long duration = timeInmillisec / 1000;
+			int hours = (int)(duration / 3600);
+			int minutes = (int) ((duration - hours * 3600)/ 60);
+			int seconds = (int) (duration - (hours * 3600 + minutes * 60));
+			
+			current_video_hours = hours;
+			current_video_minutes = minutes;
+			current_video_seconds = seconds;
+			
+			String time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+			timeText2.setText(time);
 			
 			/*
 			Intent intent = new Intent(VideoStoredInSDCard.this,
@@ -231,8 +433,25 @@ public class MainActivity extends Activity {
 			intent.putExtra("videofilename", filename);
 			startActivity(intent);*/
 			
-			videoview.setVideoPath(filename);
-			videoview.start();
+
+			videoview.requestFocus();
+			
+			videoview.setOnPreparedListener(new OnPreparedListener() {
+				
+				@Override
+				public void onPrepared(MediaPlayer mp) {
+					// TODO Auto-generated method stub
+					seekBar.setMax(videoview.getDuration());
+					Toast.makeText(MainActivity.this, Integer.toString(videoview.getDuration()), Toast.LENGTH_LONG).show();
+					//seekBar.postDelayed(onEverySecond, 1000);
+					videoview.start();
+					
+					//onEverySecond.run();					
+					
+				}
+			});			
+			//videoview.start();
+			
 			
 			List.setVisibility(View.INVISIBLE);
 			LinearLayout.setVisibility(View.VISIBLE);
@@ -306,17 +525,16 @@ public class MainActivity extends Activity {
 				String time;
 				
 				long timeInmillisec = Long.parseLong(iduration);
-				
 				long duration = timeInmillisec / 1000;
 				int hours = (int)(duration / 3600);
 				int minutes = (int) ((duration - hours * 3600)/ 60);
 				int seconds = (int) (duration - (hours * 3600 + minutes * 60));
 				
 				time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+				
 
 				holder.runningTime.setText(time);
 				videocursor.moveToPosition(position);
-
 				
 				String[] proj = { MediaStore.Video.Media._ID,
 						MediaStore.Video.Media.DISPLAY_NAME,
@@ -345,11 +563,157 @@ public class MainActivity extends Activity {
 			return convertView;
 		}
 	}
+	
+	
+    @Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Toast.makeText(MainActivity.this, "onResume()", Toast.LENGTH_LONG).show();
+		if(First_Flag == 1){
+			//videoview.setVideoPath(filename);
+			//videoview.seekTo(current_position);
+			//videoview.start();
+		}
+		/*
+        this.current_position = playservice.getSeekTime();
+        this.filename = playservice.getFilePath();
+        
+        String temp_1 = Integer.toString(current_position);
+        //String temp_2 = Integer.toString(filename)
+        
+        Toast.makeText(MainActivity.this, temp_1 + " " + filename, Toast.LENGTH_LONG).show();
+        
+        stopService(intent);
+
+        videoview.setVideoPath(filename);
+        videoview.seekTo(current_position);
+        videoview.start();*/		
+	}
+
+	@Override
+    public void onRestart()
+    {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "onRestart()", Toast.LENGTH_LONG).show();
+        //unbindService(serviceConnection);
+        //mBound = false;
+        
+        if(First_Flag == 1){
+        	try {
+				this.current_position = playservice.mService.getCurrentTime();
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	
+        	String temp = Integer.toString(current_position);
+        	
+        	Toast.makeText(MainActivity.this, temp + "::::: " + filename, Toast.LENGTH_LONG).show();
+        	
+        	unbindService(serviceConnection);
+        	stopService(intent);
+        	mBound = false;
+        
+        	videoview = (VideoView)findViewById(R.id.VideoView1);
+        	videoview.setVideoPath(filename);
+        	videoview.seekTo(current_position);
+        	videoview.requestFocus();
+    	
+        	
+        	
+        	videoview.setOnPreparedListener(new OnPreparedListener(){
+        		public void onPrepared(MediaPlayer mp){
+        			seekBar.setMax(videoview.getDuration());
+					Toast.makeText(MainActivity.this, Integer.toString(videoview.getDuration()), Toast.LENGTH_LONG).show();
+					//seekBar.postDelayed(onEverySecond, 1000);
+					videoview.start();        			
+        		}
+        	});        	
+        }
+        	/*
+    		Gson gson = new Gson();
+    		String json = mPrefs.getString("intent", "");
+    		intent = gson.fromJson(json, Intent.class);
+        	
+        	this.current_position = playservice.getSeekTime();
+        	this.filename = playservice.getFilePath();
+        
+        	String temp_1 = Integer.toString(current_position);
+        	//String temp_2 = Integer.toString(filename)
+        
+        	Toast.makeText(MainActivity.this, temp_1 + " " + filename, Toast.LENGTH_LONG).show();
+        
+        	stopService(intent);
+        
+        	videoview.setVideoPath(filename);
+        	videoview.seekTo(current_position);
+        	videoview.start();
+        	*/
+    }
+     
+    @Override	// 액티비티 -> onPause
+    public void onPause()
+    {
+    	super.onPause();
+    	Toast.makeText(MainActivity.this, "onPause()", Toast.LENGTH_LONG).show();
+    	videoview.pause();
+        int current_position = videoview.getCurrentPosition();
+
+        First_Flag = 1;
+        videoview.pause();
+
+        intent = new Intent(this, PlayService.class);
+        intent.putExtra("currentTime", current_position);
+        intent.putExtra("path", filename);
+        startService(intent);
+        bindService(intent, this.serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+     
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "onDestroy()", Toast.LENGTH_LONG).show();
+    }
+     
+    @Override
+    public void onStop()
+    {
+    	super.onStop();
+    	Toast.makeText(MainActivity.this, "onStop()", Toast.LENGTH_LONG).show();
+    	
+    }	
 
 	static class ViewHolder {
 		TextView txtTitle;
 		TextView txtSize;
 		ImageView thumbImage;
 		TextView runningTime;
-	}	
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+		super.onRestoreInstanceState(savedInstanceState);
+		Toast.makeText(MainActivity.this, "onRestoreInstanceState()", Toast.LENGTH_LONG).show();
+	} 
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		
+		super.onSaveInstanceState(outState);
+		
+		Toast.makeText(MainActivity.this, "onSaveInstanceState()", Toast.LENGTH_LONG).show();
+		outState.putString("videopath", this.filename);
+		outState.putInt("seektime", this.current_position);
+		
+		Editor prefsEditor = mPrefs.edit();
+		Gson gson = new Gson();
+		String json = gson.toJson(intent);
+		prefsEditor.putString("intent", json);
+		prefsEditor.commit();
+	} 
 }
